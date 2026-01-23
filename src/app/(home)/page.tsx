b712bf8cc7d0b47e34af1e0b8ff8bb5a507a50ef
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2026-01-23 12:08:28 +0700

    first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>Load Video Here</div>;
+}
---
+++ src/app/(home)/protected/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Only authorized users can see this page.</div>;
+};
+
+export default Page;
